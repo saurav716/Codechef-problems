17599805d249e480fe17b906af97533a452c1973
Saurav Baral Joy <159685421+saurav716@users.noreply.github.com>
2024-11-13 23:49:15 +0600

    Add files via upload

---
+++ Cricket Match.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <cmath> 
+using namespace std;
+
+int main() {
+    int T;
+    cin >> T;
+    
+    while (T--) {
+        int N, X; 
+        cin >> N >> X;
+        
+        int subscriptions_needed = (N + 5) / 6;
+        
+        int total_cost = subscriptions_needed * X;
+        
+        cout << total_cost << endl;
+    }
+
+    return 0;
+}
