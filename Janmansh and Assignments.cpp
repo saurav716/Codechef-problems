e2fbca8bc648fe10af5d5187b25a699430a2c057
Saurav Baral Joy <159685421+saurav716@users.noreply.github.com>
2024-11-14 22:04:34 +0600

    Add files via upload

---
+++ Janmansh and Assignments.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int T;
+    cin >> T;
+
+    while (T--) {
+        int X;
+        cin >> X;
+
+        if (X + 3 <= 10) {
+            cout << "Yes" << endl;
+        } else {
+            cout << "No" << endl;
+        }
+    }
+
+    return 0;
+}
