711ad489e9569e11eda016668cba5a33362387e2
Saurav Baral Joy <159685421+saurav716@users.noreply.github.com>
2024-11-20 19:57:47 +0600

    Add files via upload

---
+++ Jenga Night.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int T;
+    cin >> T;
+
+    while (T--) {
+        int N;
+        cin >> N;
+        
+        int cars = (N + 3) / 4;
+        cout << cars << endl;
+    }
+
+    return 0;
+}
