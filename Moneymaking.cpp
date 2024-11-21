a347ab12c57e342ed3b75f2b6b8d0573676059fd
Saurav Baral Joy <159685421+saurav716@users.noreply.github.com>
2024-11-21 18:39:03 +0600

    Add files via upload

---
+++ Moneymaking.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int X, Y;
+    cin >> X >> Y;
+
+    int totalMoney = (X * 5000) + (Y * 9800);
+
+    cout << totalMoney << endl;
+
+    return 0;
+}
