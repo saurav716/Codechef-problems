d0fe09a42855c8b50b6b41d2d3e6cc5c77e13b1f
Saurav Baral Joy <159685421+saurav716@users.noreply.github.com>
2024-11-24 19:41:06 +0600

    Add files via upload

---
+++ Gymkhana Election IIIT-A.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+
+int main() {
+    ll T;
+    cin >> T;
+    while (T--) {
+        ll N, M;
+        cin >> N >> M;
+        ll votes_needed = (M/2+ 1);
+        cout << votes_needed << endl;
+    }
+    return 0;
+}
