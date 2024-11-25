2d71db87ddede8dfd3f023dcc19798e4845b810e
Saurav Baral Joy <159685421+saurav716@users.noreply.github.com>
2024-11-25 18:38:42 +0600

    Add files via upload

---
+++ Flip the cards.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int T;
+    cin >> T;
+
+    while (T--) {
+        int N, X;
+        cin >> N >> X;
+        cout << min(X, N - X) << endl;
+    }
+
+    return 0;
+}
