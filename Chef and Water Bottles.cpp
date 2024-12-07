b542d316f4bad26cb74a65b4a616ca6cc252af15
Saurav Baral Joy <159685421+saurav716@users.noreply.github.com>
2024-12-07 20:00:40 +0600

    Add files via upload

---
+++ Chef and Water Bottles.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--){
+	int a,b,c;
+	cin>>a>>b>>c;
+	int x=c/b;
+	cout<< min(x,a)<<endl;
+	}
+	return 0;
+
+}
